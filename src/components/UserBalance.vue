fb57cb076a060c0682dea06f123d2cf1736598d2
Didinzon Valderrama <arboledadv@gmail.com>
2020-12-16 15:46:41 -0500

    heroku prueba

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,7 +20,7 @@
           this.username = this.$route.params.username
 
           let self = this
-          axios.get("https://cajero-app-14.herokuapp.com/user/balance/" + this.username)
+          axios.get("https://cajero-api-14.herokuapp.com/user/balance/" + this.username)
           //axios.get("http://localhost:8000/user/balance/" + this.username)
               .then((result) => {
                   self.balance = result.data.balance
